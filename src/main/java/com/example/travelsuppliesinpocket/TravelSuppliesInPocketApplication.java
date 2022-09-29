0aed19b4cb25c68cfad0bc49c9920bfcd95097d7
ikohong <ikohong88@gmail.com>
2022-09-30 01:16:01 +0900

    feat : Create MainPage

---
+++ src/main/java/com/example/travelsuppliesinpocket/TravelSuppliesInPocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelsuppliesinpocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSuppliesInPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSuppliesInPocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travelsuppliesinpocket/TravelSuppliesInPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelsuppliesinpocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSuppliesInPocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
